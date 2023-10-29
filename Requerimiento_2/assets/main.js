ba2eca1b42343b70e07704cf37ddec81bb4b2c9c
Mikel Ballesteros <mikelballes1@gmail.com>
2023-10-29 20:08:40 +0100

    Requerimiento 1 y Requerimiento 2 Completos

--- Requerimiento_2/assets/main.js
+++ Requerimiento_2/assets/main.js
@@ -1,123 +1,38 @@
 
+//creamos este evento para que la funcion se ejecute una vez el html se haya cargado completamente.
+document.addEventListener("DOMContentLoaded", function() {
+    var botonProcesarPedido = document.getElementById("procesarPedido");//obtenemos el valor procesar pedido y se le asigna la funcion botonProcesarPedido
+    botonProcesarPedido.addEventListener("click", calcularPrecio);///agregamos el boton click y se le pasa la funcion calcularPrecio
 
-//Preparamos el formulario para cuando tenga botón de enviar
-//form.onsubmit = validarInputs;
-
-
-
-window.onload = function () {
-
-    /*-- Todas las referencias al DOM que utilizamos --*/
-    const form = document.getElementById("form_pedido"),
-        t_pequena = document.getElementById("pequena"),
-        t_mediana = document.getElementById("mediana"),
-        t_grande = document.getElementById("grande"),
-        btn_enviar = document.querySelector("#btn-enviar"),
-        btn_procesar = document.querySelector("#btn-procesar"),
-        ingr = document.querySelectorAll("input[name='ingredientes']"),
-        inputs = document.querySelectorAll("input"),
-        inputsTamano = document.getElementsByName("tamano");
-
-    /*---*/
-
-
-
-    /**
-     * Calcula cuál es el precio total en función de las opciones que 
-     * ha marcado el usuario en el formulario. 
-     * El precio base son: 
-     * - 5 (pizza pequeña)
-     * - 10 (Piza mediana)
-     * - 15 (Pizza grande)
-     * 
-     * Sobre ese precio se suma +1€ por cada ingrediente seleccionado
-     * 
-     * No se comprueba si no hay ninguna pizza seleccionada porque
-     * ya se valida el formulario antes de enviarlo y porque por defecto
-     * se marca la pequeña
-     * @returns {int} Devuelve el precio Total
-     */
-    function comprobarPrecio() {
-        let precioTotal = 0;
-
-        if (t_pequena.checked == true)
-            precioTotal = 5;
-        else if (t_mediana.checked == true)
-            precioTotal = 10;
-        else
-            precioTotal = 15;
-
-        for (ele of ingr) {
-            if (ele.checked)
-                precioTotal += 1;
-        }
-        return precioTotal;
-    }
-
-    /**
-     * Añade un párrafo al final del formulario indicando cuál es el precio
-     * del pedido.
-     * 
-     * Cada vez que se ejecuta, se sustituye el párrafo anterior por el nuevo
-     */
-    function generarTextoPrecio() {
-
-        //Limpiamos el párrafo donde se encuentra el precio de la pizza
-        const precio_pizza = document.querySelector("#precio_pizza")
-
-        if (precio_pizza != null) {
-            precio_pizza.parentNode.removeChild(precio_pizza);
-        }
-
-        //Generamos el nodo que contiene el precio actual de la pizza y lo insertamos.
-        const node_precio = document.createElement("p");
-        node_precio.id = "precio_pizza";
-        const texto_precio = document.createTextNode(`El precio de tu pedido es ${comprobarPrecio()} €`);
-
-        node_precio.appendChild(texto_precio);
-        form.appendChild(node_precio);
-
-    }
-
-    /**
-     * Se validan todos los inputs del formulario: 
-     * - Type: texto, tel, email: Se comprueba que no están vacíos
-     * - Radio: Se comprueba que hay una opción marcada.
-     * @returns {boolean} Devuelve false si la validación no se cumple y
-     *                    true si lo damos por validado
-     */
-    function validarInputs() {
-        //Validamos los inputs de tipo texto email y teléfono
-        const tiposInputAValidar = ["text", "email", "tel"];
-        for (let inp of inputs) {
-            if (tiposInputAValidar.includes(inp.type)) {
-                if (inp.value.trim() == "") {
-                    alert(`ERROR. Todos los campos de texto tienen que estar cumplimentados. Campo pendiente: ${inp.name}`);
-                    return false;
-                }
-            }
-        }
-
-        //Validamos los inputs radio
-        let rad_select = false
-        for (let rad of inputsTamano) {
-            if (rad.checked)
-                rad_select = true;
-            break;
-        }
-        if (!rad_select)
+    
+    function calcularPrecio() {
+       //// Se verifica que los campos nombre,direccion,telefono y email estan  completados, en el caso de que esten vacios salta el error (debe rellenar los campos)
+        if (nombre.value.trim() == "" || direccion.value.trim()=="" || telefono.value.trim()=="" || email.value.trim()=="") {
+            alert('[ERROR] Debe rellenar todos los campos');
             return false;
+        }
+    
+        var tamaño = document.querySelector('input[name="tamaño"]:checked');//obtenemos el valor de la variable tamaño.
+        var ingredientes = document.querySelectorAll('input[name="ingredientes"]:checked'); //obtenemos el valor de los ingredientes seleccionados. 
+        var precioTotalElement = document.getElementById("precioTotal");//Obtenemos el elemento precioTotal
 
-        // Si no hemos devuelto false antes. Damos el formulario por validado
-        return true;
+    
+       //- if--En caso de que se haya seleccionado al menos un tamaño y recorremos los ingrediente para saber si al menos uno ha sido elegido, se le suma el valor de cada ingrediente
+       //mas el del temaño y se muestra el precio total en el elemento precioTotal.
+       //Si no se ha seleccionado un tamaño o algun ingrediente, sale el alert(Por favor, seleccione al menos un tamaño y un ingrediente.")
+
+        if (tamaño && ingredientes.length > 0) {
+            let precio = parseInt(tamaño.value);
+            
+    
+            for (let ingrediente of ingredientes) {
+                precio += parseInt(ingrediente.value);
+            }
+
+            precioTotalElement.textContent = precio + " €";
+        } else {
+            precioTotalElement.textContent = "0 €";
+            alert("Por favor, seleccione al menos un tamaño y un ingrediente.");
+        }
     }
-
-    //Lanzmos la comrpobación al pulsar el botón Procesar
-    btn_procesar.addEventListener("click", function () {
-        generarTextoPrecio();
-        validarInputs(); //De momento añadimos aquí la validación
-    })
-
-    //Prearamos el formulario para cuando tenga botón ENVIAR
-    //form.onsubmit = validarInputs;
-}
+});
