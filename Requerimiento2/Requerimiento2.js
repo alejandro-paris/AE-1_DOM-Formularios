13c57b7e4540454ca7c13cb727c6b0e92c2a9c32
Mikel Ballesteros <mikelballes1@gmail.com>
2023-10-29 19:45:09 +0100

    terminado

--- Requerimiento2/Requerimiento2.js
+++ Requerimiento2/Requerimiento2.js
@@ -2,18 +2,19 @@ document.addEventListener("DOMContentLoaded", function() {
     var botonProcesarPedido = document.getElementById("procesarPedido");
     botonProcesarPedido.addEventListener("click", calcularPrecio);
 
-    function validacionformulario() {
+    
+    function calcularPrecio() {
 
-       
         if (nombre.value.trim() == "" || direccion.value.trim()=="" || telefono.value.trim()=="" || email.value.trim()=="") {
             alert('[ERROR] Debe rellenar todos los campos');
             return false;
         }
-    }
-    function calcularPrecio() {
+        
         var tamaño = document.querySelector('input[name="tamaño"]:checked');
         var ingredientes = document.querySelectorAll('input[name="ingredientes"]:checked');
         var precioTotalElement = document.getElementById("precioTotal");
+
+    
 
         if (tamaño && ingredientes.length > 0) {
             let precio = parseInt(tamaño.value);
